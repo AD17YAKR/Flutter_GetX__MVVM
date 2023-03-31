e5672462aa48e0b8f514d204ecf33b77bba5496c
AD17YAKR <adityasingh.110601@gmail.com>
2023-03-31 23:15:10 +0530

    ADD: Basic Getx Setup and an Example for Getx

---
+++ lib/homseScreen.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_getx_mvvm/counterController.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CounterController counterController = CounterController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Obx(
+        () => Text(
+          counterController.counter.toString(),
+          style: TextStyle(fontSize: 52),
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterController.incrementCounter();
+        },
+        child: Icon(Icons.abc),
+      ),
+    );
+  }
+}
---
+++ lib/languages.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: override_on_non_overriding_member, non_constant_identifier_names, annotate_overrides
+
+import 'package:get/get_navigation/src/root/internacionalization.dart';
+
+class Languages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en-US': {'message': 'What is Your Name', "name": 'Aditya kumar Singh'}
+      };
+}
